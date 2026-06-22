e23e7e89da690d85d0f973ffbb0c1ce1f3c9758f
huasanli2 <1619256619@qq.com>
2026-06-22 19:56:51 +0800

    fix: use heredoc for python shell command to avoid quoting issues

--- build.sh
+++ build.sh
@@ -8,14 +8,14 @@ echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
 echo "Creating and updating users..."
-python manage.py shell -c "
+python manage.py shell << 'EOF'
 from django.contrib.auth import get_user_model
 User = get_user_model()
-for uname, nick, staff in [('user1', '\u5b9d\u5b9d', True), ('user2', '\u8d1d\u8d1d', False)]:
+for uname, nick, staff in [('user1', '宝宝', True), ('user2', '贝贝', False)]:
     u, created = User.objects.get_or_create(username=uname, defaults={'nickname': nick, 'is_staff': staff, 'is_superuser': staff})
     u.set_password('123456')
     u.save()
-"
+EOF
 
 echo "Starting server..."
 exec daphne config.asgi:application --port $PORT --bind 0.0.0.0
